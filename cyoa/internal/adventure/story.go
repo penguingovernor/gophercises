5b2001e03849a3c5d1ecd2c939c7d4750f3d9214
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-04-20 21:46:50 +0000

    Add: HTTP handler

--- cyoa/internal/adventure/story.go
+++ cyoa/internal/adventure/story.go
@@ -2,7 +2,11 @@ package adventure
 
 import (
 	"encoding/json"
+	"fmt"
+	"html/template"
 	"io"
+	"net/http"
+	"strings"
 )
 
 type Story map[string]Storyblock
@@ -25,3 +29,61 @@ func LoadStory(rd io.Reader) (*Story, error) {
 	}
 	return &st, nil
 }
+
+func (s Story) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.New("").Parse(storyHTML))
+
+	resource := strings.TrimFunc(r.URL.Path, func(letter rune) bool {
+		return letter == '/'
+	})
+
+	path, found := s[resource]
+	if !found {
+		http.Error(w, fmt.Sprintf("We couldn't find %s, sorry bud :/", r.URL.Path), http.StatusNotFound)
+		return
+	}
+	tmpl.Execute(w, path)
+}
+
+const storyHTML = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Choose your adventure!</title>
+</head>
+
+<body>
+
+  <div id="CurrentStory">
+    <h1 id="StoryTitle">
+      {{.Title}}
+    </h1>
+    {{range .Paragraphs}}
+    <p class="StoryText">
+      {{.}}
+    </p>
+    {{end}}
+  </div>
+
+  <div id="StoryOptions">
+    <label for="storyOptions">Choose your adventure:</label>
+    <select id="storyOptions">
+      {{range .Options}}
+      <option value="{{.Arc}}">{{.Arc}}</option>
+      {{end}}
+    </select>
+    <button onclick="goToPath()">Go!</button>
+
+  </div>
+  <script>
+    const goToPath = () => {
+      const selectElement = document.getElementById('storyOptions')
+      const path = selectElement.options[selectElement.selectedIndex].value
+      window.location.replace(path)
+    }
+  </script>
+</body>
+
+</html>`
--- cyoa/main.go
+++ cyoa/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/penguingovernor/gophercises/cyoa/internal/adventure"
+)
+
 func main() {
+	storyFile := flag.String("story_json", "./data/gopher.json", "the json story to load")
+	flag.Parse()
+	storyFD, err := os.Open(*storyFile)
+	if err != nil {
+		log.Fatalf("failed to load story: %v\n", err)
+	}
+	defer storyFD.Close()
+
+	story, err := adventure.LoadStory(storyFD)
+	if err != nil {
+		log.Fatalf("failed to parse story: %v\n", err)
+	}
+
+	if err := http.ListenAndServe("localhost:8080", story); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 
 }
